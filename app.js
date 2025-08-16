05e88998999495d937e3e23d9e7b65123d293245
Anjalidevasani <anjalidevasani10@gmail.com>
2025-08-16 14:06:35 +0530

    2 nd question

--- app.js
+++ app.js
@@ -29,3 +29,36 @@ else{
     finalPrice = normalPrice;
     console.log(finalPrice);
 }
+
+
+// Question 2:code
+// inputs
+let orderAmount = 800;
+let customerIsPremiumMember = true;
+let isWeekdayBetweenNight = true;
+let couponApplied = false;
+let sundayMorning = true;
+let finalAmount = orderAmount;
+// case 1: if orderAmount greater than 1000 they get discount
+if(orderAmount>1000 && customerIsPremiumMember){
+    finalAmount = orderAmount - (orderAmount*15/100);
+    console.log("Benefit: Free Delivary and 15% Discount")
+    console.log("Final amount to pay:", finalAmount);
+}
+//  case 2: if orderAmount between 500 to 1000 and weekday get discount
+else if(orderAmount>=500 && orderAmount<=1000 && isWeekdayBetweenNight && !couponApplied){
+    finalAmount = orderAmount - (100);
+    console.log("Benefit: 100 ruppes discount")
+    console.log("Final amount to pay:", finalAmount);
+}
+// case 3: sunday morning
+else if(orderAmount<500 && sundayMorning ){
+    finalAmount = orderAmount;
+    console.log("Benefit: Free Delivary ")
+    console.log("Final amount to pay:", finalAmount);
+}
+else{
+    finalAmount = orderAmount
+    console.log("Benefit: none")
+    console.log("Final amount to pay:", finalAmount);
+}
